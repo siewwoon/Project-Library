ddc628e1ae801664186329093bce81aa58c6255a
siewwoon <siewwoon.lee@gmail.com>
2020-11-16 23:24:27 +0800

    initial submission
    initial submission

---
+++ App.js
@@ -0,0 +1,57 @@
+import React from 'react';  
+import './App.css';  
+import BookSearch from "/Users/siewwoon/Mages/FS102/project/library/src/Component/Search Bar/BookSearch.js";
+import Footer from "./Component/Footer/Footer.js";
+import "./Component/Footer/Footer.css";
+import ControlledCarousel from './Component/Slider/Slider';
+import PopularList from "/Users/siewwoon/Mages/FS102/project/library/src/Component/Popular/PopularList.js";
+import Navbar from '/Users/siewwoon/Mages/FS102/Project-Library/project/library/src/Component/NavBar/Navbar.js';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Catergories from "/Users/siewwoon/Mages/FS102/Project-Library/project/library/src/Component/NavBar/Catergories.js";
+import ContactUs from "/Users/siewwoon/Mages/FS102/Project-Library/project/library/src/Component/NavBar/ContactUs.js";
+import SignUp from "/Users/siewwoon/Mages/FS102/Project-Library/project/library/src/Component/NavBar/SignUp.js";
+import Home from "./Users/siewwoon/Mages/FS102/Project-Library/project/library/src/Component/NavBar/Home.js";
+
+
+  
+function App() {  
+  return (  
+    <>
+    <Router>
+ <Navbar />
+<Switch>
+  <Route path='/' exact component={Home} />
+  <Route path='/catergories' exact component={Catergories} />
+  <Route path='/contactUs' exact component={ContactUs} />
+  <Route path='/SignUp' exact component={SignUp} />
+
+
+
+
+</Switch>
+
+
+
+  </Router>
+
+    <div>
+      <ControlledCarousel />
+    </div>
+
+    <div>
+    <BookSearch /> 
+    </div>
+
+    <div>
+      <PopularList />
+    </div>
+
+    <div>
+      <Footer />
+      </div> 
+
+    </>
+  )  
+}  
+  
+export default App; 
---
+++ Component/Popular/PopularList.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";  
+import axios from 'axios';  
+import { Card, Button } from 'react-bootstrap';
+import "/Users/siewwoon/Mages/FS102/project/library/src/Component/Popular/PopularList.css";  
+import Modal from "react-modal";
+
+function Popular() {  
+    const [book, setBook] = useState("");  
+    const [result, setResult] = useState([]);  
+ 
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=subject:fiction&key=AIzaSyDQfceMVLzrKegjreG5O7JrPngWxDz55Xw&maxResults=30")  
+        .then(data => {  
+            console.log(data.data.items);  
+            setResult(data.data.items);  
+        })      
+
+ function KnowMore ()  {
+     const [modalisOpen, setModalIsOpen] = useState(false);
+ }      
+
+ return (
+    <div className="containerb">  
+    <div className="cell">  
+        {result.map(book => (  
+            <div className="col-sm-3">  
+                <Card className="Card">  
+
+                    <Card.Img className="img" variant="top" src={book.volumeInfo.imageLinks !== undefined ? book.volumeInfo.imageLinks.thumbnail : 'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'} alt={book.title} />  
+                    <Card.Body>  
+                        <h5 className="card-title">{book.volumeInfo.title}</h5>  
+                        <Button className="btn btn-primary"  onClick={() => setModalIsOpen (true)}>Know more</Button>  
+                        <Modal isOpen ={modalIsOpen}> 
+                        <h2> Modal title</h2>
+                        <p> Modal Body</p>
+                        <div>
+                            <Button onClick={() => {
+                                    return setModalIsOpen(false);
+                                }}>Close</Button>
+                        </div>
+                        </Modal>
+                    </Card.Body>  
+                </Card>  
+            </div>  
+        ))}  
+    </div>  
+</div>  
+ )}      
+export default Popular
