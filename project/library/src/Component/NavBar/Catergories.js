9f5783016f52b47e98606f808453c0a97d01ca6b
siewwoon <siewwoon.lee@gmail.com>
2020-11-21 11:08:37 +0800

    adding updated files

--- project/library/src/Component/NavBar/Catergories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Catergories () {
-    return (
-        <>
-<h1 className="Categories">Catergories </h1>
-        </>
-    )
-}
--- project/library/src/Component/NavBar/ContactUs.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ContactUs () {
-    return (
-        <>
-<h1 className="contactUs">Contact Us </h1>
-        </>
-    )
-}
