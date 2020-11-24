3e0131e2b2deb1acd70b8d9dc00ccd6142038d71
siewwoon <siewwoon.lee@gmail.com>
2020-11-24 13:59:35 +0800

    updated book search

--- src/Component/Search Bar/BookSearch.js
+++ src/Component/Search Bar/BookSearch.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";  
 import axios from 'axios';  
-import { Card } from 'react-bootstrap';  
+import { Card, Button } from 'react-bootstrap'; 
 import "./BookSearch.css";
+import Modal from "react-modal";
+import Heart from "react-heart";
+
 
 
 const apiKey = "AIzaSyDQfceMVLzrKegjreG5O7JrPngWxDz55Xw";
@@ -25,6 +28,8 @@ function BookSearch() {
     }  
 
 
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
 
     return (  
         <form onSubmit={handleSubmit}>  
@@ -46,10 +51,19 @@ function BookSearch() {
   
                                 <Card.Img className="img" variant="top" src={book.volumeInfo.imageLinks !== undefined ? book.volumeInfo.imageLinks.thumbnail : 'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'} alt={book.title} />  
                                 <Card.Body>  
-                                    <h5 className="card-title">{book.volumeInfo.title}</h5>  
-                                    <a className="btn btn-primary" >Know more</a>  
-                                
+                                    <h5 className="card-title">{book.volumeInfo.title}</h5> 
+                                    <Heart /> 
+                                    <Button className="btn btn-primary"  onClick={() => setModalIsOpen(true)}>Know more</Button> 
+                        <Modal isOpen ={modalIsOpen}> 
+                        <h2> {book.volumeInfo.description}</h2>
+                        <div>
+                            <Button onClick={() => {
+                                    return setModalIsOpen(false);
+                                }}>Close</Button>
+                        </div>
+                        </Modal>
                                 </Card.Body>  
+
                             </Card>  
                         </div>  
                     ))}  
